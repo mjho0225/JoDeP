4f7f724d2c460830cce45c5b864bab0d08ad7160
mjho0225 <52567731+mjho0225@users.noreply.github.com>
2019-08-12 11:38:36 +0900

    스크립트 테스트

--- MiniProject/Assets/02.Scripts/TESTScripts.cs
+++ MiniProject/Assets/02.Scripts/TESTScripts.cs
@@ -13,6 +13,7 @@ public class TESTScripts : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("@@$(*!(@*$");
+        Debug.Log("@@$(*!(@*$!!!");
     }
 }
